698b2550297cf7b1938ef55fd9c78e7e4184d149
dinamagdyrashed <dinarashid78@gmail.com>
2024-09-02 20:53:32 +0300

    finish

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
-import { SharedModule } from '../../shared/shared.module';
+import { NgClass } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { ProductsService } from '../service/products.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NgClass],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss',
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  isNavbarOpen = false;
+  getMe: boolean = false;
+
+  constructor(private getState: ProductsService) {}
+
+  toggleNavbar() {
+    this.isNavbarOpen = !this.isNavbarOpen;
+  }
+
+  ngOnInit(): void {
+    this.getMe = this.getState.getMe;
+    console.log(this.getMe);
+    this.getState.loadState();
+  }
+}
--- src/app/component/product-details/product-details.component.ts
+++ src/app/component/product-details/product-details.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit, Pipe } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductsService } from '../service/products.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CurrencyPipe, NgClass, NgFor } from '@angular/common';
+import { StarRatingModule } from 'angular-star-rating';
+import { CartService } from '../service/cart.service';
 
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, CurrencyPipe, NgClass, StarRatingModule],
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.scss'
+  styleUrl: './product-details.component.scss',
+  providers: [ProductsService],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  productId: number = 0;
+  product: any;
 
+  constructor(
+    private route: ActivatedRoute,
+    private service: ProductsService,
+    private cart: CartService
+  ) {}
+  getProduct() {
+    this.service
+      .getSpecificProducts(this.productId)
+      .subscribe((res) => ((this.product = res), console.log(this.product)));
+  }
+  ngOnInit(): void {
+    this.productId = +this.route.snapshot.paramMap.get('id')!;
+    this.getProduct();
+    console.log(this.product);
+  }
+
+  addToCart(prod: any) {
+    this.cart.addToCart(prod);
+  }
 }
